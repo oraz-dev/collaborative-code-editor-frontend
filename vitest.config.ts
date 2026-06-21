f0ee2b4ef19d180bd164087cc9dc159c5b97235d
Orazmyrat-Hojamyradov <yakupyakupbow197@gmail.com>
2026-06-21 23:04:12 +0500

    chore: add vitest and testing library setup

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+
+export default defineConfig({
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test-setup.ts'],
+    css: { modules: { classNameStrategy: 'non-scoped' } },
+  },
+})
