1c39a476dc8c33eeef11b20ac6f62f7e627197e8
Orazmyrat-Hojamyradov <yakupyakupbow197@gmail.com>
2026-06-22 22:09:44 +0500

    feat: add classNames helper

---
+++ src/shared/lib/classNames/classNames.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export type Mods = Record<string, boolean | undefined>;
+
+export function classNames(
+  cls: string,
+  mods: Mods = {},
+  additional: (string | undefined)[] = [],
+): string {
+  return [
+    cls,
+    ...additional.filter(Boolean),
+    ...Object.entries(mods)
+      .filter(([_, value]) => value)
+      .map(([className]) => className),
+  ].join(' ');
+}
